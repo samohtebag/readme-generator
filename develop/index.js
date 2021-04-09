05b6bb2963c866030f46385462bff55a5ab9ae1f
gabe thomas <gabe.thomas@bizzabo.com>
2021-04-08 23:32:52 -0400

    finished generateMarkdown file, starting js

--- develop/index.js
+++ develop/index.js
@@ -1,4 +1,7 @@
 // TODO: Include packages needed for this application
+const inquirerNPM = require("inquirer");
+const fsNPM = require ("fs");
+const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
 const questions = [];
--- develop/utils/generateMarkdown.js
+++ develop/utils/generateMarkdown.js
@@ -20,16 +20,77 @@ function renderLicenseBadge(license) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license === "apache-2.0") {
+    return `(https://www.apache.org/licenses/LICENSE-2.0)`
+  } else if (license === 'BSD 3-Clause "New" or "Revised"') {
+    return `(https://choosealicense.com/licenses/bsd-3-clause/)`
+  } else if (license === 'GNU General Public License v3.0') {
+    return `(https://www.gnu.org/licenses/gpl-3.0.en.html)`
+  } else if (license === 'MIT') {
+    return `(https://github.com/git/git-scm.com/blob/main/MIT-LICENSE.txt)`
+  } else {
+    return ""
+  }
+};
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license) {
+    return '## License'
+  } else {
+    return ""
+  }
+};
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
+  ${renderLicenseBadge(data.license)};
+  ${renderLicenseLink(data.license)};
+
+  ## Description
+
+  ${data.description}
+
+  ## Table of Contents
+    * [Installation](#installation)
+    * [Usage](#usage)
+    * [License](#license)
+    * [Contributions](#contributions)
+    * [Tests](#tests)
+    * [Questions](#questions)
+
+  
+  ## Installation
+
+  ${data.installation}
+
+  ## Usage
+
+  ${data.usage}
+
+  ## Links
+
+  * Live/Deployed Application: (https://${data.username}.github.io/${data.repository}/)
+  * Github Repo: (https://github.com/${data.username}/${data.repository}/)
+
+  ## Contributions
+
+  ${data.contributions}
+
+  ## Tests
+
+  ${data.tests}
+
+  ## Questions
+
+  [![GitHub](https://img.shields.io/badge/Github-Click%20Here-orange)](https://github.com/${data.username}) 
+  [![LinkedIn](https://img.shields.io/badge/LinkedIn-Click%20Here-yellowgreen)](https://www.linkedin.com/in/gabriel-thomas513/)
+
+  You may contact me via email ($[data.email]) with any questions regarding this project or anything else you see!
 `;
 }
 
